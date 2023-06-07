f716d7dc520f03b2718ef9fa73a28e09fb9f4100
MartynoSiela <m.satas@gmail.com>
2023-06-08 00:22:17 +0300

    Add project files.

---
+++ Framework/Common.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+
+namespace Framework
+{
+    internal static class Common
+    {
+        private static IWebElement GetElement(string xpath)
+        {
+            return Driver.Instance.FindElement(By.XPath(xpath));
+        }
+
+        private static IList<IWebElement> GetElements(string xpath)
+        {
+            return Driver.Instance.FindElements(By.XPath(xpath));
+        }
+
+        internal static void OpenPage(string url)
+        {
+            Driver.Instance.Url = url;
+        }
+
+        internal static void ClickElement(string xpath)
+        {
+            GetElement(xpath).Click();
+        }
+
+        internal static void SendKeysToElement(string xpath, string keys)
+        {
+            GetElement(xpath).SendKeys(keys);
+        }
+
+        internal static string GetElementText(string xpath)
+        {
+            return GetElement(xpath).Text;
+        }
+    }
+}
---
+++ Framework/Driver.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Framework
+{
+    public static class Driver
+    {
+        internal static IWebDriver Instance { get; private set; }
+
+        public static void Initialize()
+        {
+            Instance = new ChromeDriver();
+        }
+
+        public static void Quit()
+        {
+            Instance.Quit();
+        }
+    }
+}
